30a208dd0d69495295b4898d7bab617bd2fb41f2
theroamer <st880221@gmail.com>
2025-06-12 01:24:10 +0800

    feat: fulfill suspense & loading for fetching data (chapter 8~9)

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,9 +1,13 @@
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import React from 'react';
 
-export default function Page() {
+export default async function Page() {
   return (
     <div>
       <p>Invoices Page</p>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <LatestInvoices />
+      </div>
     </div>
   );
 };
