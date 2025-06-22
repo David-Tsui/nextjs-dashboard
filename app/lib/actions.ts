87b2a2666e0ef748b5f03e4dac9a1c545d53e6b5
theroamer <st880221@gmail.com>
2025-06-22 16:25:58 +0800

    feat: enhance create invoice form with server side validation and error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -13,18 +13,43 @@ const fixedAmount = (amount: number, multiplier = 100) => {
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    required_error: 'Please select a customer.',
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    required_error: 'Please select an invoice status.',
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-  const rawFormData = Object.fromEntries(formData.entries());
-  const { customerId, amount, status } = CreateInvoice.parse(rawFormData);
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
 
+export async function createInvoice(prevState: State, formData: FormData) {
+  const rawFormData = Object.fromEntries(formData.entries());
+  const validatedFields = CreateInvoice.safeParse(rawFormData);
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = fixedAmount(amount);
   const date = new Date().toISOString().split('T')[0];
 
@@ -35,8 +60,11 @@ export async function createInvoice(formData: FormData) {
     `;
 
   } catch (error) {
+    // If a database error occurs, return a more specific error.
     console.error('Error::[createInvoice]:', error);
-    throw error;
+    return {
+      message: 'Database Error: Failed to create invoice.',
+    };
   }
 
   revalidatePath('/dashboard/invoices');
--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { CustomerField } from '@/app/lib/definitions';
 import Link from 'next/link';
 import {
@@ -6,10 +8,15 @@ import {
   CurrencyDollarIcon,
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
+import type { State } from '@/app/lib/actions';
 import { createInvoice } from '@/app/lib/actions';
 import SubmitButton from '../submit-button';
+import { useActionState } from 'react';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+  const initialState: State = { message: null, errors: {} };
+  const [state, formAction] = useActionState(createInvoice, initialState);
+
   if (!customers || customers.length === 0) {
     return (
       <div className="rounded-md bg-red-50 p-4 text-red-700">
@@ -20,89 +27,130 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
 
   const renderCustomerSelection = () => (
     <>
-      <select
-        id="customer"
-        name="customerId"
-        className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-        defaultValue=""
-        required
-      >
-        <option value="" disabled>
-          Select a customer
-        </option>
-        {customers.map((customer) => (
-          <option key={customer.id} value={customer.id}>
-            {customer.name} ({customer.email})
+      <div className="relative">
+        <select
+          id="customer"
+          name="customerId"
+          className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+          defaultValue=""
+          aria-describedby="customer-error"
+        >
+          <option value="" disabled>
+            Select a customer
           </option>
-        ))}
-      </select>
-      <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          {customers.map((customer) => (
+            <option key={customer.id} value={customer.id}>
+              {customer.name} ({customer.email})
+            </option>
+          ))}
+        </select>
+        <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+      </div>
+      <div id="customer-error" aria-live="polite" aria-atomic="true">
+        {
+          state.errors?.customerId &&
+          state.errors.customerId.map((error: string) => (
+            <p className="mt-2 text-sm text-red-500" key={error}>
+              {error}
+            </p>
+          ))
+        }
+      </div>
     </>
   );
 
   const renderAmountInput = () => (
     <>
-      <input
-        id="amount"
-        name="amount"
-        type="number"
-        step="0.01"
-        placeholder="Enter USD amount"
-        className="peer block w-full rounded-md border border-gray-200 py-2 pl-11 text-sm outline-2 placeholder:text-gray-500"
-        required
-      />
-      <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+      <div className="relative">
+        <input
+          id="amount"
+          name="amount"
+          type="number"
+          step="0.01"
+          placeholder="Enter USD amount"
+          className="peer block w-full rounded-md border border-gray-200 py-2 pl-11 text-sm outline-2 placeholder:text-gray-500"
+        />
+        <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+      </div>
+      <div
+        id="amount-error"
+        aria-live="polite"
+        aria-atomic="true"
+      >
+        {
+          state.errors?.amount &&
+          state.errors.amount.map((error: string) => (
+            <p className="mt-2 text-sm text-red-500" key={error}>
+              {error}
+            </p>
+          ))
+        }
+      </div>
     </>
   );
 
   const renderStatusRadioButtons = () => (
-    <div className="flex gap-4">
-      <div className="flex items-center">
-        <input
-          id="pending"
-          name="status"
-          type="radio"
-          value="pending"
-          className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
-          defaultChecked
-          required
-        />
-        <label
-          htmlFor="pending"
-          className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-gray-100 px-3 py-1.5 text-xs font-medium text-gray-600"
-        >
-          Pending <ClockIcon className="h-4 w-4" />
-        </label>
+    <>
+      <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
+        <div className="relative flex gap-4">
+          <div className="flex items-center">
+            <input
+              id="pending"
+              name="status"
+              type="radio"
+              value="pending"
+              className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+            />
+            <label
+              htmlFor="pending"
+              className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-gray-100 px-3 py-1.5 text-xs font-medium text-gray-600"
+            >
+              Pending <ClockIcon className="h-4 w-4" />
+            </label>
+          </div>
+          <div className="flex items-center">
+            <input
+              id="paid"
+              name="status"
+              type="radio"
+              value="paid"
+              className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+            />
+            <label
+              htmlFor="paid"
+              className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-green-500 px-3 py-1.5 text-xs font-medium text-white"
+            >
+              Paid <CheckIcon className="h-4 w-4" />
+            </label>
+          </div>
+        </div>
       </div>
-      <div className="flex items-center">
-        <input
-          id="paid"
-          name="status"
-          type="radio"
-          value="paid"
-          className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
-        />
-        <label
-          htmlFor="paid"
-          className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-green-500 px-3 py-1.5 text-xs font-medium text-white"
-        >
-          Paid <CheckIcon className="h-4 w-4" />
-        </label>
+      <div
+        id="status-error"
+        aria-live="polite"
+        aria-atomic="true"
+      >
+        {
+          state.errors?.status &&
+          state.errors.status.map((error: string) => (
+            <p className="mt-2 text-sm text-red-500" key={error}>
+              {error}
+            </p>
+          ))
+        }
       </div>
-    </div>
+    </>
   );
 
   return (
-    <form action={createInvoice}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
             Choose customer
           </label>
-          <div className="relative">
-            {renderCustomerSelection()}
-          </div>
+          {renderCustomerSelection()}
         </div>
 
         {/* Invoice Amount */}
@@ -111,9 +159,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             Choose an amount
           </label>
           <div className="relative mt-2 rounded-md">
-            <div className="relative">
-              {renderAmountInput()}
-            </div>
+            {renderAmountInput()}
           </div>
         </div>
 
@@ -122,11 +168,18 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
           <legend className="mb-2 block text-sm font-medium">
             Set the invoice status
           </legend>
-          <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
-            {renderStatusRadioButtons()}
-          </div>
+          {renderStatusRadioButtons()}
         </fieldset>
       </div>
+
+      <div className="mt-4 form-error">
+        {state.message && (
+          <div className="mt-4 rounded-md bg-red-50 p-4 text-red-600">
+            {state.message}
+          </div>
+        )}
+      </div>
+
       <div className="mt-6 flex justify-end gap-4">
         <Link
           href="/dashboard/invoices"
@@ -134,10 +187,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
         >
           Cancel
         </Link>
-        <SubmitButton
-          formAction={createInvoice}
-          pendingText="Creating..."
-        >
+        <SubmitButton pendingText="Creating...">
           Create Invoice
         </SubmitButton>
       </div>
